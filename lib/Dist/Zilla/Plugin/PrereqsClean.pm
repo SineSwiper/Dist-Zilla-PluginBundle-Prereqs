5de803e813eeb8894fb934a46488dac7ad3636ae
Brendan Byrd <Perl@ResonatorSoft.org>
2012-07-16 16:34:43 -0400

    Initial commit

---
+++ lib/Dist/Zilla/Plugin/PrereqsClean.pm
@@ -0,0 +1,20 @@
+package Dist::Zilla::Plugin::PrereqsClean;
+
+# VERSION
+# ABSTRACT: ---insert abstract here---
+
+use sanity;
+
+### INSERT CODE HERE ###
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+ 
+   # code
+ 
+=head1 DESCRIPTION
+ 
+Insert description here...
